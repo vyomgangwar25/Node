8ec737a272e28957f6857b66c0cc2f82aaaa3b3e
vyomgangwar25 <vyomgangwar25@gmail.com>
2023-09-21 18:02:38 +0530

    create static html page using express

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-const fs=require('fs')
-const path=require('path');
-const dirPath=path.join(__dirname,'CURD') //It gives path of CURD folder
-// console.log(dirPath)
-const filePath=`${dirPath}/apple.txt`;//it gives(create) path of file in curd folder
-// fs.writeFileSync(filePath,'this is simple text file') create file
+const express =require('express');
+const path=require('path')
+const app=express();
+const publicPath=path.join(__dirname,'public')
 
+// app.use(express.static(publicPath))
+app.get('',(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/About.html`)
+})
+app.get('/help',(req,res)=>{
+    res.sendFile(`${publicPath}/Help.html`)
+})
+app.get('*',(req,res)=>{
+    res.sendFile(`${publicPath}.404page.html`)
+})
 
-
-//READ FILE
-// fs.readFile(filePath,'utf-8',(err,item)=>{
-//     console.log(item) 
-// })
-
-
-
-
-// UPDATE FILE 
-//  fs.appendFile(filePath,'and file is updated',(err)=>{
-//     if(!err)
-//     {
-//         console.log("file")
-//     }
-//  })
-
-
-// RENAME
-// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
-//     if(!err)
-//     {
-//         console.log("renamed")
-//     }
-// })
-
-//DELETE FILE
-fs.unlinkSync(`${dirPath}/fruit.txt`)
+app.listen(5000);
